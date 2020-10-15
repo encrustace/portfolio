6b37e9d27f1525ee799a9de887db328e22c1f626
Imran Khan <encrustace@gmail.com>
2020-10-15 13:45:18 +0530

    New things

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Box from "./Box";
 
 function App() {
+  var items = [];
+  for (let index = 0; index < 4; index++) {
+    items.push(<Box />);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <div className="App-body">
+        <div className="App-row-one">
+          {items}
+        </div>
+        <div className="App-row-two">
+          {items}
+        </div>
+      </div>
+      <footer className="App-footer">
+        <c>© Encrsut Software</c>
+      </footer>
     </div>
   );
 }
