3a182348ea2b546f1c038998b601650141916b43
Imran Khan <encrustace@gmail.com>
2024-03-07 12:58:44 +0530

    css changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/favicon.svg" sizes="any" />
       </head>
       <body className={inter.className}>
         <Navbar />
