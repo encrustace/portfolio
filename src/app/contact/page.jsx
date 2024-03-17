b7f980c5a544489af35a6e93e2a51811659d5126
Imran Khan <encrustace@gmail.com>
2024-03-17 12:22:34 +0530

    added contact me page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,25 @@
-import NotFound from "../not-found";
 import styles from "./page.module.css";
+import { contact } from "@/utils/data";
 
 export default function ContactPage() {
   return (
     <div className={styles.main}>
-      <NotFound />
+      <div className={styles.address}>
+        <p className={styles.addressHeader}>Reach me:</p>
+        <p className={styles.text}>Name: <span>{contact.name}</span></p>
+        <p className={styles.text}>Email: <span>{contact.email}</span></p>
+        <p className={styles.text}>Country: <span>{contact.country}</span></p>
+        <p className={styles.text}>Planet: <span>{contact.planet}</span></p>
+        <p className={styles.text}>Public key:<div className={styles.publicKey}><p>{contact.publikKey}</p></div></p>
+      </div>
+      <div className={styles.form}>
+        <p>Let's chat:</p>
+        <input className={styles.input} placeholder="Name" />
+        <input className={styles.input} placeholder="Email" />
+        <input className={styles.input} placeholder="Phone" />
+        <textarea className={styles.message} placeholder="Message" />
+        <button className={styles.sendBtn}>Send (Coming soon...)</button>
+      </div>
     </div>
   );
 }
--- src/utils/data.js
+++ src/utils/data.js
@@ -11,6 +11,22 @@ const texts = {
   about: "I'm a software developer specializing in building user friendly digital platforms, using ReactJS, NextJS, Angular, have a look at skills section"
 }
 
+const contact = {
+  name: "Imran Khan",
+  email: "encrustace@gmail.com",
+  country: "India",
+  planet: "Earth",
+  publikKey: `-----BEGIN PUBLIC KEY-----
+  MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwgBg8ZxbZVchTdI3uA1B
+  m5Honq5OjXLUdIk5upKGTy5cOQ6R6Wja0612P4IvaZDXpvaXxAuAkuW6+5lzriVM
+  OBaKBGrB23/XDwP8trkMANrZlmgsQyoE6Q83871y5/uaLXFpnTYLTDVPug4ny9Jz
+  mBLgtuVQElPbVBaMBCknO7J9Vpk4Ei7NHiKH1FR4CrGhMPzMZ6AHIas5P9Wn2GGv
+  k+VdLmuVlbnZvxwAzhjdF8q+0/j4qPSNYdPwnuZBwIXCgcJm/Fo7/PjavVjM1OFa
+  6mRFjPZLlIgvXB3lSrQ/yYFgpX25BplPdIkV4bbnudtFpLZRNuM2ujIFjbNn0fQm
+  1QIDAQAB
+  -----END PUBLIC KEY-----`
+}
+
 const skills = [
   "HTML",
   "JavaScript",
@@ -28,4 +44,4 @@ const skills = [
   "SQL"
 ]
 
-export { links, skills, texts };
+export { links, skills, texts, contact };
