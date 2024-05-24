38ccab13610f0b793219efdc5e249a623e5de4c4
Imran Khan <encrustace@gmail.com>
2024-05-24 21:21:31 +0530

    added rain background

--- src/app/relax/page.jsx
+++ src/app/relax/page.jsx
@@ -8,6 +8,59 @@ export default function Relax() {
     const tentAudioRef = useRef(null);
     const thunderAudioRef = useRef(null);
     const [playing, setPlaying] = useState(false);
+    const canvasRef = useRef(null)
+    // Create array to hold raindrops
+    var raindrops = [];
+
+    // Function to create raindrops
+    function createRaindrop() {
+        return {
+            x: Math.random() * canvasRef.current.width * 10,
+            y: 0,
+            length: Math.random() * 20 + 10,
+            speed: Math.random() * 10 + 10
+        };
+    }
+
+    // Function to update raindrops
+    function updateRaindrops() {
+        for (var i = 0; i < raindrops.length; i++) {
+            var raindrop = raindrops[i];
+            raindrop.y += raindrop.speed;
+            if (canvasRef.current && raindrop.y > canvasRef.current.height) {
+                raindrop.y = 0;
+            }
+        }
+    }
+
+    // Function to draw raindrops
+    function drawRaindrops() {
+        if (canvasRef.current) {
+            var ctx = canvasRef.current.getContext('2d');
+            // Set canvas size
+            canvasRef.current.width = window.innerWidth;
+            canvasRef.current.height = window.innerHeight;
+
+            // ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+            for (var i = 0; i < raindrops.length; i++) {
+                var raindrop = raindrops[i];
+                ctx.beginPath();
+                ctx.moveTo(raindrop.x, raindrop.y);
+                ctx.lineTo(raindrop.x, raindrop.y + raindrop.length);
+                ctx.strokeStyle = 'white';
+                ctx.stroke();
+            }
+        }
+    }
+    function initRainDrop() {
+        for (var i = 0; i < 500; i++) {
+            raindrops.push(createRaindrop());
+        }
+        setInterval(function () {
+            updateRaindrops();
+            drawRaindrops();
+        }, 50);
+    }
 
     const playPauseMaster = () => {
         if (tentAudioRef.current) {
@@ -47,23 +100,26 @@ export default function Relax() {
     }
 
     useEffect(() => {
-
-    })
+        initRainDrop();
+    }, [])
 
 
     return (
         <div className={styles.main}>
-            <audio ref={tentAudioRef} onTimeUpdate={()=>handleTimeUpdate(tentAudioRef, 13)}>
-                <source src={`/audios/tent_rain.ogg`} type="audio/ogg" />
-            </audio>
-            <audio ref={thunderAudioRef} onTimeUpdate={()=>handleTimeUpdate(thunderAudioRef, 11)}>
-                <source src={`/audios/thunder_rain.ogg`} type="audio/ogg" />
-            </audio>
-            <div className={styles.soundsBox}>
-                <button className={styles.button} onClick={() => playPause(tentAudioRef)}><FontAwesomeIcon className={styles.soundButton} style={{ color: "red" }} icon={faTent} /></button>
-                <button className={styles.button} onClick={() => playPause(thunderAudioRef)}><FontAwesomeIcon className={styles.soundButton} icon={faThunderstorm} /></button>
+            <canvas className={styles.canvas} ref={canvasRef}></canvas>
+            <div className={styles.musicBox}>
+                <audio ref={tentAudioRef} onTimeUpdate={() => handleTimeUpdate(tentAudioRef, 13)}>
+                    <source src={`/audios/tent_rain.ogg`} type="audio/ogg" />
+                </audio>
+                <audio ref={thunderAudioRef} onTimeUpdate={() => handleTimeUpdate(thunderAudioRef, 11)}>
+                    <source src={`/audios/thunder_rain.ogg`} type="audio/ogg" />
+                </audio>
+                <div className={styles.soundsBox}>
+                    <button className={styles.button} onClick={() => playPause(tentAudioRef)}><FontAwesomeIcon className={styles.soundButton} style={{ color: "red" }} icon={faTent} /></button>
+                    <button className={styles.button} onClick={() => playPause(thunderAudioRef)}><FontAwesomeIcon className={styles.soundButton} icon={faThunderstorm} /></button>
+                </div>
+                <button className={styles.button} onClick={playPauseMaster}><FontAwesomeIcon className={styles.playPauseBtn} icon={playing ? faCirclePause : faCirclePlay} /></button>
             </div>
-            <button className={styles.button} onClick={playPauseMaster}><FontAwesomeIcon className={styles.playPauseBtn} icon={playing ? faCirclePause : faCirclePlay} /></button>
         </div>
     );
 };
